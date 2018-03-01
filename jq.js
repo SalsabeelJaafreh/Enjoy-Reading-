a7d3ad18908dd241d49802754020761ae924efb3
sebo1234567 <s.jaafreh@yahoo.com>
2018-03-01 10:59:23 +0200

    add more books

--- jq.js
+++ jq.js
@@ -6,14 +6,12 @@ function addDivBook(books,divName){
           var book = books[index];
           var secRow=$('<div class="row divbooks"></div>');
           var thirdCol=$('<div class="col-xs-5 marg"></div>');
-          var fourthCol=$('<div class="col-xs-3 marg"> </div>');
-          var fifthCol=$('<div class="col-xs-4 marg"></div>');
-          var image=$('<img src="' + books[index].bookImage + '">');
+          var fifthCol=$('<div class="col-xs-7 marg"></div>');
+          var image=$('<img src="' + books[index].bookImage + '" class="imageSize">');
           var data=$('<h3>'+ books[index].bookTitle +'</h3><h5>'+books[index].author +'<p>('+ books[index].genres +')</p></h4><p>'+ books[index].quote+'</p>');
           fifthCol.append(data);
           thirdCol.append(image);
           secRow.append(thirdCol);
-          secRow.append(fourthCol);
           secRow.append(fifthCol);
           divName.append(secRow);
           index -= 1;
@@ -28,22 +26,28 @@ addDivBook(streams.books,$showBooks);
 $('#searchBtn').on('click', function () {
          var searchTxt= $("#searchTxt").val()
          var bookArr=search(searchTxt);
+         if(searchTxt===""){
+            $showBooks.html("")
+            addDivBook(streams.books,$showBooks);
+          }
          if(bookArr.length>0){
-          addDivBook(bookArr,$$showBooks )
-        
-          }         
+          $showBooks.html("")
+          addDivBook(bookArr,$showBooks )
+          }
+
 });
 $( "#searchTxt" ).keyup(function() {
  var searchTxt= $("#searchTxt").val()
          var bookArr=search(searchTxt);
+         if(searchTxt===""){
+            $showBooks.html("")
+            addDivBook(streams.books,$showBooks);
+          }
          if(bookArr.length>0){
           $showBooks.html("")
           addDivBook(bookArr,$showBooks )        
           } 
-          if(searchTxt===""){
-            $showBooks.html("")
-            addDivBook(streams.books,$showBooks);
-          }     
+               
 });
 
 });
